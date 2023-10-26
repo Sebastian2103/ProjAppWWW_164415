ee8efcc35636bfe8cf5b25e90b9d4c5f05149049
Sebastian Pająk <42960067+Sebastian2103@users.noreply.github.com>
2023-10-26 23:49:58 +0200

    lab3

---
+++ lab1_3/js/show.js
@@ -0,0 +1,66 @@
+document.getElementById("academy").addEventListener("click", function() {
+    var ukrytyKontent = document.getElementById("academy_h");
+    if (ukrytyKontent.style.display === "none") {
+      ukrytyKontent.style.display = "flex";
+    } else {
+      ukrytyKontent.style.display = "none";
+    }
+  });
+
+document.getElementById("necropolis").addEventListener("click", function() {
+    var ukrytyKontent = document.getElementById("necropolis_h");
+    if (ukrytyKontent.style.display === "none") {
+      ukrytyKontent.style.display = "flex";
+    } else {
+      ukrytyKontent.style.display = "none";
+    }
+});
+document.getElementById("dungeon").addEventListener("click", function() {
+    var ukrytyKontent = document.getElementById("dungeon_h");
+    if (ukrytyKontent.style.display === "none") {
+      ukrytyKontent.style.display = "flex";
+    } else {
+      ukrytyKontent.style.display = "none";
+    }
+});
+document.getElementById("sylvan").addEventListener("click", function() {
+    var ukrytyKontent = document.getElementById("sylvan_h");
+    if (ukrytyKontent.style.display === "none") {
+      ukrytyKontent.style.display = "flex";
+    } else {
+      ukrytyKontent.style.display = "none";
+    }
+});
+document.getElementById("haven").addEventListener("click", function() {
+    var ukrytyKontent = document.getElementById("haven_h");
+    if (ukrytyKontent.style.display === "none") {
+      ukrytyKontent.style.display = "flex";
+    } else {
+      ukrytyKontent.style.display = "none";
+    }
+});
+document.getElementById("inferno").addEventListener("click", function() {
+    var ukrytyKontent = document.getElementById("inferno_h");
+    if (ukrytyKontent.style.display === "none") {
+      ukrytyKontent.style.display = "flex";
+    } else {
+      ukrytyKontent.style.display = "none";
+    }
+});
+document.getElementById("stronghold").addEventListener("click", function() {
+    var ukrytyKontent = document.getElementById("stronghold_h");
+    if (ukrytyKontent.style.display === "none") {
+      ukrytyKontent.style.display = "flex";
+    } else {
+      ukrytyKontent.style.display = "none";
+    }
+});
+document.getElementById("fortress").addEventListener("click", function() {
+    var ukrytyKontent = document.getElementById("fortress_h");
+    if (ukrytyKontent.style.display === "none") {
+      ukrytyKontent.style.display = "flex";
+    } else {
+      ukrytyKontent.style.display = "none";
+    }
+});
+
